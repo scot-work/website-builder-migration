78b593e539412b1d8e426e7dc1da123068725c2e
Scot Close <scotclose@Belphanior.local>
2014-03-12 16:27:52 -0700

    initial add

---
+++ test-top-ten.sh
@@ -0,0 +1,15 @@
+rm -rf ./people/*
+rm -rf ./errors/*
+rm out.txt
+python migrate.py steven.lee >> out.txt
+python migrate.py nancy.stork >> out.txt
+python migrate.py anand.vaidya >> out.txt
+python migrate.py guadalupe.salazar >> out.txt
+python migrate.py quincy.mccrary >> out.txt
+python migrate.py mary.juno >> out.txt
+python migrate.py carol.mukhopadhyay >> out.txt
+python migrate.py tsau.lin >> out.txt
+python migrate.py marcos.pizarro >> out.txt
+python migrate.py gordon.haramaki >> out.txt
+cd people
+zip -r archive.zip *
